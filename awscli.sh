9e58e0ddba9313a033b808866589d18439000c60
sanjukuruvilla <kuruvillasanju@gmail.com>
2024-03-25 18:09:05 +0000

    updated script for installing awscli

---
+++ awscli.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Function to install AWS CLI version 2 on Linux x86 (64-bit)
+install_awscli_linux_x86_64() {
+    # Download AWS CLI installation package
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    # Unzip the package
+    unzip awscliv2.zip
+    # Run the installation script
+    sudo ./aws/install
+}
+
+# Function to install AWS CLI version 2 on Linux ARM
+install_awscli_linux_arm() {
+    # Download AWS CLI installation package
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-arm64.zip" -o "awscliv2.zip"
+    # Unzip the package
+    unzip awscliv2.zip
+    # Run the installation script
+    sudo ./aws/install
+}
+
+# Check Linux OS architecture
+if [ "$(uname -m)" == "x86_64" ]; then
+    # Install AWS CLI for Linux x86 (64-bit)
+    install_awscli_linux_x86_64
+elif [ "$(uname -m)" == "aarch64" ]; then
+    # Install AWS CLI for Linux ARM
+    install_awscli_linux_arm
+else
+    echo "Unsupported Linux architecture. Please install AWS CLI manually."
+    exit 1
+fi
+
