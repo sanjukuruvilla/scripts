4faeb3127976422fc14278d4c71f54df69a94005
EC2 Default User <ec2-user@ip-172-31-38-158.ap-northeast-3.compute.internal>
2024-03-25 17:25:41 +0000

    updated scripts

--- TF.sh
+++ TF.sh
@@ -1,19 +1,51 @@
 #!/bin/bash
 
-# Install prerequisites
-sudo apt update
-sudo apt install -y unzip
+# Function to install Terraform on Debian-based systems
+install_terraform_debian() {
+    # Update package lists
+    sudo apt update
+    # Install unzip utility
+    sudo apt install -y unzip
+    # Download latest version of Terraform
+    TERRAFORM_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f 4)
+    wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    # Unzip Terraform binary
+    unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    # Move Terraform binary to /usr/local/bin directory
+    sudo mv terraform /usr/local/bin/
+    # Verify installation
+    terraform --version
+}
 
-# Download the latest version of Terraform
-TERRAFORM_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f 4)
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+# Function to install Terraform on RHEL-based systems
+install_terraform_rhel() {
+    # Install EPEL repository
+    sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    # Install unzip utility
+    sudo yum install -y unzip
+    # Download latest version of Terraform
+    TERRAFORM_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f 4)
+    wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    # Unzip Terraform binary
+    unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    # Move Terraform binary to /usr/local/bin directory
+    sudo mv terraform /usr/local/bin/
+    # Verify installation
+    terraform --version
+}
 
-# Unzip the Terraform binary
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-
-# Move the Terraform binary to /usr/local/bin directory
-sudo mv terraform /usr/local/bin/
-
-# Verify installation
-terraform --version
+# Check Linux OS version
+if [ -f /etc/debian_version ]; then
+    # Debian-based system
+    install_terraform_debian
+elif [ -f /etc/redhat-release ]; then
+    # RHEL-based system
+    install_terraform_rhel
+elif grep -qi "Amazon Linux" /etc/system-release; then
+    # Amazon Linux
+    install_terraform_rhel
+else
+    echo "Unsupported Linux distribution. Please install Terraform manually."
+    exit 1
+fi
 
