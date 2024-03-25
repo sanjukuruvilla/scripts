f299d4c9f943a557d192d8e99e3c24d9e8046a53
sanjukuruvilla <kuruvillasanju@gmail.com>
2024-03-25 17:37:07 +0000

    updated the scripts

--- TF.sh
+++ TF.sh
@@ -2,36 +2,32 @@
 
 # Function to install Terraform on Debian-based systems
 install_terraform_debian() {
-    # Update package lists
-    sudo apt update
-    # Install unzip utility
-    sudo apt install -y unzip
-    # Download latest version of Terraform
-    TERRAFORM_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f 4)
-    wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-    # Unzip Terraform binary
-    unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-    # Move Terraform binary to /usr/local/bin directory
-    sudo mv terraform /usr/local/bin/
-    # Verify installation
-    terraform --version
+    # Add HashiCorp GPG key
+    wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    # Add HashiCorp repository
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    # Update package lists and install Terraform
+    sudo apt update && sudo apt install -y terraform
 }
 
 # Function to install Terraform on RHEL-based systems
 install_terraform_rhel() {
-    # Install EPEL repository
-    sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-    # Install unzip utility
-    sudo yum install -y unzip
-    # Download latest version of Terraform
-    TERRAFORM_VERSION=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d '"' -f 4)
-    wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-    # Unzip Terraform binary
-    unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-    # Move Terraform binary to /usr/local/bin directory
-    sudo mv terraform /usr/local/bin/
-    # Verify installation
-    terraform --version
+    # Install yum-utils for yum-config-manager
+    sudo yum install -y yum-utils
+    # Add HashiCorp repository
+    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+    # Install Terraform
+    sudo yum -y install terraform
+}
+
+# Function to install Terraform on Amazon Linux
+install_terraform_amazon_linux() {
+    # Install yum-utils and shadow-utils
+    sudo yum install -y yum-utils shadow-utils
+    # Add HashiCorp repository
+    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+    # Install Terraform
+    sudo yum -y install terraform
 }
 
 # Check Linux OS version
@@ -43,7 +39,7 @@ elif [ -f /etc/redhat-release ]; then
     install_terraform_rhel
 elif grep -qi "Amazon Linux" /etc/system-release; then
     # Amazon Linux
-    install_terraform_rhel
+    install_terraform_amazon_linux
 else
     echo "Unsupported Linux distribution. Please install Terraform manually."
     exit 1
